4090f4ca32351f4e8fc6e57ba5d2e955ea081c62
arpitnath <arpitnath42@gmail.com>
2021-07-05 03:24:27 +0530

    break

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Navbar } from './Navbar'
 export { default as Jumbotron } from './Jumbotron'
+export { default as Break } from './Break'
--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Jumbotron } from '../containers'
+import { Break } from '../components'
 
 const Board = () => {
   return (
     <main>
       <Jumbotron />
+      <Break />
     </main>
   )
 }
