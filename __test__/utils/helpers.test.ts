5428e92cad215417723df5abf9722f309ebb0182
arpitnath <arpitnath42@gmail.com>
2021-07-08 19:24:05 +0530

    unit test for helpers: moveInsideCurrentList

--- __test__/utils/helpers.test.ts
+++ __test__/utils/helpers.test.ts
@@ -1,5 +1,8 @@
 import { data } from '../../src/utils/defData'
-import { moveToDifferentGroup } from '../../src/utils/helpers'
+import {
+  moveInsideCurrentList,
+  moveToDifferentGroup
+} from '../../src/utils/helpers'
 import { expect } from 'chai'
 import { Data, Group, Task } from '../../src/utils/types'
 
@@ -68,6 +71,10 @@ describe('MOVE TASK TO A DIFFERENT GROUP', () => {
       newtargetGroup = newState[targetGroupRef]
     })
 
+    after(() => {
+      prevState = undefined
+    })
+
     it('New State should have same length after moving the task to a new group', () => {
       const lengthPrevState = prevState.length
       const lengthNewState = copyOfPrevState.length
@@ -108,6 +115,61 @@ describe('MOVE TASK TO A DIFFERENT GROUP', () => {
         newTragetTaskList[newTragetTaskList.length - 1].id
 
       expect(lastTaskInTargetTaskList).to.be.eql(currentTaskId)
+    })
+  })
+})
+
+//next
+describe('MOVE TASK IN THE SAME GROUP', () => {
+  let listOfTask: undefined | Data = undefined
+  before(() => {
+    listOfTask = JSON.parse(JSON.stringify(data))
+  })
+
+  describe('should move a task from a current index to a target index in the same group ', () => {
+    let prevState: undefined | Data = undefined
+    let copyOfPrevState: undefined | Data = undefined
+    let newState: undefined | Data = undefined
+
+    let groupRef: undefined | number = undefined
+    let fromIndex: undefined | number = undefined
+    let targetIndex: undefined | number = undefined
+
+    before(() => {
+      prevState = listOfTask
+      copyOfPrevState = JSON.parse(JSON.stringify(listOfTask))
+
+      groupRef = 1
+      fromIndex = 2
+      targetIndex = 0
+
+      newState = moveInsideCurrentList(
+        copyOfPrevState,
+        groupRef,
+        fromIndex,
+        targetIndex
+      )
+    })
+
+    after(() => {
+      prevState = undefined
+    })
+
+    it('new list should be of same length of the target group list', () => {
+      const prevStateTargetGroupList = prevState[groupRef].tasks
+      const newStateTargetGroupList = newState[groupRef].tasks
+
+      expect(prevStateTargetGroupList.length).to.be.eql(
+        newStateTargetGroupList.length
+      )
+    })
+
+    it('target task should be at target index of the list', () => {
+      const newStateTargetGroupList = newState[groupRef].tasks
+      const currentTaskId = prevState[groupRef].tasks[fromIndex].id
+      const taskAtTargetIndexId = newStateTargetGroupList[targetIndex].id
+
+      expect(taskAtTargetIndexId).to.be.eq(currentTaskId)
     })
   })
 })
