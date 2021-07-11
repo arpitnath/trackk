a06fba8d16b8406b422364fd58e1357634d60e05
arpitnath <arpitnath42@gmail.com>
2021-07-11 21:41:46 +0530

    fake loader

--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Jumbotron, Groups } from '../containers'
 import { Break } from '../components'
+import Loader from '../components/Loader'
 
 const Board: React.FC = () => {
+  const [fakeState, setFakeState] = React.useState(true)
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setFakeState(false)
+    }, 700)
+  }, [])
+
   return (
     <main>
-      <Jumbotron />
-      <Break />
-      <Groups />
+      {fakeState ? (
+        <Loader />
+      ) : (
+        <>
+          <Jumbotron />
+          <Break />
+          <Groups />
+        </>
+      )}
     </main>
   )
 }
